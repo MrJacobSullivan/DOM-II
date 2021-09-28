9f95750256766845e261d9a90e40e5bdb6a80fab
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-09-28 13:23:46 -0500

    added keydown event to change h1 red

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ bus.addEventListener('mouseover', (e) => {
   e.target.style.transition = '100ms'
   e.target.style.transform = 'translate(50px)'
 })
+
+// keydown
+const h1 = document.querySelector('h1')
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') h1.style.color = 'red'
+})
